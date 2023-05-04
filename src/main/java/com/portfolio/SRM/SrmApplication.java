f3a80fe87ee6d31116eeb1de7a4e2fb7910b8b4a
matias <matias.sillen@gmail.com>
2023-05-04 10:44:24 -0300

    Primer commit

---
+++ src/main/java/com/portfolio/SRM/SrmApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.SRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrmApplication.class, args);
+	}
+
+}
