044789b4050ce68ed05bb0f0514db919bef8117a
matias <matias.sillen@gmail.com>
2023-05-14 12:12:43 -0300

    Creé y configure la clase Proyecto para cada uno de los paquetes

---
+++ src/main/java/com/portfolio/SRM/Dto/dtoProyecto.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.SRM.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ *
+ * @author Matías Sillen
+ */
+public class dtoProyecto {
+    @NotBlank
+    private String nombreP;
+    @NotBlank
+    private String descripcionP;
+    
+    public dtoProyecto() {
+    }
+
+    public dtoProyecto(String nombreP, String descripcionP) {
+        this.nombreP = nombreP;
+        this.descripcionP = descripcionP;
+    }
+
+    public String getNombreP() {
+        return nombreP;
+    }
+
+    public void setNombreP(String nombreP) {
+        this.nombreP = nombreP;
+    }
+
+    public String getDescripcionP() {
+        return descripcionP;
+    }
+
+    public void setDescripcionP(String descripcionP) {
+        this.descripcionP = descripcionP;
+    }
+}
