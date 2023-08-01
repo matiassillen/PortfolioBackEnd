c945a8f924a7c717bba3bd9af85558b410c7ce3e
matiassillen <121519299+matiassillen@users.noreply.github.com>
2023-08-01 03:13:07 -0300

    Update AuthController.java

--- src/main/java/com/portfolio/SRM/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/SRM/Security/Controller/AuthController.java
@@ -4,7 +4,6 @@
  */
 package com.portfolio.SRM.Security.Controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import com.portfolio.SRM.Security.Dto.JwtDto;
 import com.portfolio.SRM.Security.Dto.LoginUsuario;
 import com.portfolio.SRM.Security.Dto.NuevoUsuario;
@@ -53,7 +52,6 @@ public class AuthController {
     @Autowired
     JwtProvider jwtProvider;
     
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/nuevo")
     public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
         if(bindingResult.hasErrors())
